196f4506234a8db395c0d1e3d662ed181f1c39b2
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2020-12-01 14:55:26 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,12 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :"views/super_hero"
+    end
+
+    post '/teams' do
+        erb :"views/team"
+    end
 
 end
